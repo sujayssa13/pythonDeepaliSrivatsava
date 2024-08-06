d93e71a36288d477cbf2c9dcf97e23c5f46d5bdd
sujayssa13 <sujay.ssa13@gmail.com>
2024-08-06 12:27:24 +0530

    Create string_methods.py

---
+++ string_methods.py
@@ -0,0 +1,10 @@
+#string methods
+s = "hello"
+print(s.upper())
+print(s.title())
+print(s.swapcase())
+print(s.capitalize())
+
+q = "Durjaya is totally useless"
+print(q.capitalize())
+print(q.title())
